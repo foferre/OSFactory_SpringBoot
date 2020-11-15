09aea7392f35ae9b02d69bb03775f50e0969be03
foferre <felipe.oferreira@sonda.com>
2020-11-15 20:24:29 -0300

    First commit - Spring and Database initialized.

---
+++ src/main/java/com/felipe/osfactory/api/OsFactoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.felipe.osfactory.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsFactoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsFactoryApiApplication.class, args);
+	}
+
+}
